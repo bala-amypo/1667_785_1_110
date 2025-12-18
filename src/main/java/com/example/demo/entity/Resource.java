8288ce312f7cc9378ad0f9cca589a194aaad28fc
bala-amypo <bala@amypo.in>
2025-12-18 17:46:26 +0000

    User commit

--- src/main/java/com/example/demo/entity/Resource.java
+++ src/main/java/com/example/demo/entity/Resource.java
@@ -1,16 +1,17 @@
 package com.example.demo.entity;
 
+import java.time.LocalDat
+
 public class Resource{
     private Integer id;
     private String resourceName;
     private String resourceType;
-    @Min(1)
     private int capacity;
     private String location;
     private LocalDateTime createdAt;
 
 
-        public Long getId() {
+    public Long getId() {
         return id;
     }
 

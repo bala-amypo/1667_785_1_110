17dddd5186150907cdc033ae71bd75128431d95a
bala-amypo <bala@amypo.in>
2025-12-19 03:16:15 +0000

    User commit

--- src/main/java/com/example/demo/entity/ResourceRequest.java
+++ src/main/java/com/example/demo/entity/ResourceRequest.java
@@ -1,12 +1,24 @@
 package com.example.demo.entity;
 
-public class ResourceRequest{
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+public class ResourceRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String resourceType;
+    private User requestedBy;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    @NotNull
+    private String purpose;
+    private String status;
+    private ResourceAllocation resourceAllocation;
 
 
-    
 
-   
-
-    public ResourceRequest(){
-    }
 }

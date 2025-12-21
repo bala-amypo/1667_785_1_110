219572c064c92c3945256e674ad7a80b4c253482
bala-amypo <bala@amypo.in>
2025-12-21 13:43:32 +0000

    User commit

--- src/main/java/com/example/demo/entity/ResourceRequest.java
+++ src/main/java/com/example/demo/entity/ResourceRequest.java
@@ -1,1 +1,35 @@
-a
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class ResourceRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String resourceType;
+    @ManyToOne
+    @JoinColumn(name = "requested_by", nullable = false)
+    private User requestedBy;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    @Column(nullable = false)
+    private String purpose;
+
+    private String status;
+
+    @PrePersist
+    public void prePersist() {
+        if (this.status == null || this.status.isEmpty()) {
+            this.status = "PENDING";
+        }
+    }
+}

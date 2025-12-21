e8c4bf33a4befe838ebc789d83cfdcab72a0610b
bala-amypo <bala@amypo.in>
2025-12-21 05:32:11 +0000

    User commit

--- src/main/java/com/example/demo/controller/ResourceController.java
+++ src/main/java/com/example/demo/controller/ResourceController.java
@@ -1,1 +1,26 @@
-package
+package com.example.demo.controller;
+
+import com.example.demo.entity.Resource;
+import com.example.demo.service.ResourceService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ResourceController {
+
+    @PostMapping("/api/resources")
+    public Resource create(@RequestBody Resource resource) {
+        return resourceService.createResource(resource);
+    }
+
+    @GetMapping("/api/resources")
+    public List<Resource> getAll() {
+        return resourceService.getAllResources();
+    }
+
+    @GetMapping("/api/resources/{id}")
+    public Resource getById(@PathVariable Long id) {
+        return resourceService.getResource(id);
+    }
+}

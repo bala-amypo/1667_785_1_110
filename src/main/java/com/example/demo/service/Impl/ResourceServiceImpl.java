80ffec9f6510229d1136a52464386651a23766cb
bala-amypo <bala@amypo.in>
2025-12-21 05:26:12 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Impl/ResourceServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.Resource;
+import com.example.demo.repository.ResourceRepository;
+import com.example.demo.service.ResourceService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ResourceServiceImpl implements ResourceService {
+
+    private final ResourceRepository resourceRepository;
+
+    public ResourceServiceImpl(ResourceRepository resourceRepository) {
+        this.resourceRepository = resourceRepository;
+    }
+
+    @Override
+    public Resource createResource(Resource resource) {
+
+        if (resourceRepository.existsByResourceName(resource.getResourceName())) {
+            throw new RuntimeException("Resource already exists");
+        }
+
+        return resourceRepository.save(resource);
+    }
+
+    @Override
+    public Resource getResource(Long id) {
+        return resourceRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Resource not found"));
+    }
+
+    @Override
+    public List<Resource> getAllResources() {
+        return resourceRepository.findAll();
+    }
+}

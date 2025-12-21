e201dc76c0dcf1974cac1e89091b9e8bc60da86b
bala-amypo <bala@amypo.in>
2025-12-21 17:31:52 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Impl/AllocationRuleServiceImpl.java
@@ -0,0 +1,42 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.AllocationRule;
+import com.example.demo.repository.AllocationRuleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AllocationRuleServiceImpl implements AllocationRuleService {
+
+    private final AllocationRuleRepository allocationRuleRepository;
+
+    public AllocationRuleServiceImpl(AllocationRuleRepository allocationRuleRepository) {
+        this.allocationRuleRepository = allocationRuleRepository;
+    }
+
+    @Override
+    public AllocationRule createRule(AllocationRule rule) {
+
+        if (allocationRuleRepository.existsByRuleName(rule.getRuleName())) {
+            throw new RuntimeException("AllocationRule with this ruleName already exists");
+        }
+
+        if (rule.getPriorityWeight() < 0) {
+            throw new RuntimeException("priorityWeight must be greater than or equal to 0");
+        }
+
+        return allocationRuleRepository.save(rule);
+    }
+
+    @Override
+    public AllocationRule getRule(Long id) {
+        return allocationRuleRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("AllocationRule not found"));
+    }
+
+    @Override
+    public List<AllocationRule> getAllRules() {
+        return allocationRuleRepository.findAll();
+    }
+}

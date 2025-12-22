23ae4d794c87809906ff099fe8bcc88e6fe9c5af
bala-amypo <bala@amypo.in>
2025-12-22 03:56:28 +0000

    User commit

--- src/main/java/com/example/demo/entity/Resource.java
+++ src/main/java/com/example/demo/entity/Resource.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.Min;
 
 import java.time.LocalDateTime;
 

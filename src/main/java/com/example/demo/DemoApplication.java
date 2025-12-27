31e1f1486d94d137e85b6dd4b5bc65d59b7f8297
bala-amypo <bala@amypo.in>
2025-12-27 04:21:08 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,14 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println(new BCryptPasswordEncoder().encode("12345"));
+
+		BCryptPasswordEncoder encoder= new BCryptPasswordEncoder();
+		if(encoder.matches("12345","$2a$10$1MF.hoQy3nFqNTnPpeKHPOaNF162JoAZDw5SO97BN8hxRFsELnwti")){
+			System.err.println("Password Matched");
+		}
+		else{
+			System.out.print("No match");
+		}
 	}
 
 }

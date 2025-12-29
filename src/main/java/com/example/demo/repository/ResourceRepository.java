2a292d34193044824b6b0394c133664bf7fe6779
bala-amypo <bala@amypo.in>
2025-12-29 01:33:33 +0000

    User commit

--- src/main/java/com/example/demo/repository/ResourceRepository.java
+++ src/main/java/com/example/demo/repository/ResourceRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
+import com.example.demo.entity.ResourceAllocation;
 
 
 public interface ResourceRepository extends JpaRepository<Resource, Long> {
